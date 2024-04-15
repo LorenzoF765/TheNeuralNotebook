512553b73c495bc08b6bbf9f817feddb2026cd9e
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2024-04-15 17:44:41 -0600

    Updated React and Django

---
+++ NNDir/NeuralNotebook/NeuralNotebookApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view (['GET'])
+def testView(request):
+    return Response({'response': "Hi there"})
+
+
--- nn-frontend/src/App.js
+++ nn-frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const onSubmit=async (e )=> {
     e.preventDefault();
-    console.log("Penis!!");
+    await axios.get("http://127.0.0.1:8000/test")
+      .then((response)=>{
+        console.log(response.data);
+      })
   }
+
   return (
     <div className="App">
       <form onSubmit={onSubmit}> 
