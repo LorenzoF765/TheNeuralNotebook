1126ba4594d41c6f41c5eff2f5c6c587e04ac47d
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2024-04-19 14:23:06 -0600

    Basic OpenAI Setup

--- NNDir/NeuralNotebook/NeuralNotebook/settings.py
+++ NNDir/NeuralNotebook/NeuralNotebook/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'openai',
+    'openai_test'
 ]
 
 MIDDLEWARE = [
--- NNDir/NeuralNotebook/NeuralNotebookApp/views.py
+++ NNDir/NeuralNotebook/NeuralNotebookApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from openai_test import Yippee
 
 @api_view (['GET'])
 def testView(request):
-    return Response({'response': "Hi there"})
+    response = Yippee()    
+    return Response({'response': response})
+
+
 
 
