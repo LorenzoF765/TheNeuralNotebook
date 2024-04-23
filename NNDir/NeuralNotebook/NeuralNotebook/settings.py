76d6afee6c014afc2a44c73a50165bfaf6e9f17e
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2024-04-22 21:50:40 -0600

    no more env >:(

--- NNDir/NeuralNotebook/NeuralNotebook/settings.py
+++ NNDir/NeuralNotebook/NeuralNotebook/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NeuralNotebookApp',
     'rest_framework',
     'corsheaders',
     'openai',
-    'openai_test'
 ]
 
 MIDDLEWARE = [
--- NNDir/NeuralNotebook/NeuralNotebookApp/views.py
+++ NNDir/NeuralNotebook/NeuralNotebookApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from openai_test import Yippee
+from .openai_test import Yippee
 
 @api_view (['GET'])
 def testView(request):
